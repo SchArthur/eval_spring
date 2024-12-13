025b05fc6df3e51c61a3e436282bab944bd6916a
Arthur <arthur.9996@gmail.com>
2024-12-13 10:31:06 +0100

    Added Get /entreprise/{id}

--- src/main/java/com/eval/demo/controller/EntrepriseController.java
+++ src/main/java/com/eval/demo/controller/EntrepriseController.java
@@ -3,12 +3,15 @@ package com.eval.demo.controller;
 import com.eval.demo.dao.EntrepriseDao;
 import com.eval.demo.model.Entreprise;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -26,6 +29,18 @@ public class EntrepriseController {
 
         return entrepriseDao.findAll();
 
+    }
+
+    @GetMapping("/entreprise/{id}")
+    public ResponseEntity<Entreprise> getEntrepriseById(@PathVariable int id) {
+
+        Optional<Entreprise> optionalEntreprise = entrepriseDao.findById(id);
+
+        if(optionalEntreprise.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(optionalEntreprise.get(), HttpStatus.OK);
     }
 
 }
