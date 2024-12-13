b4b5cd452217aa37ebbcfa9e99f97e730a1b3dfb
Arthur <arthur.9996@gmail.com>
2024-12-13 11:05:31 +0100

    Added CRUD for ConventionController

---
+++ src/main/java/com/eval/demo/controller/ConventionController.java
@@ -0,0 +1,95 @@
+package com.eval.demo.controller;
+
+import com.eval.demo.dao.ConventionDao;
+import com.eval.demo.model.Convention;
+import com.fasterxml.jackson.annotation.JsonView;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+public class ConventionController {
+
+    @Autowired
+    private ConventionDao conventionDao;
+
+    public ConventionController(ConventionDao conventionDao) {
+        this.conventionDao = conventionDao;
+    }
+
+    // READ
+    @GetMapping("/convention")
+    public List<Convention> getAllConvention() {
+
+        return conventionDao.findAll();
+
+    }
+
+    // READ
+    @GetMapping("/convention/{id}")
+    public ResponseEntity<Convention> getConventionById(@PathVariable int id) {
+
+        Optional<Convention> optionalConvention = conventionDao.findById(id);
+
+        if(optionalConvention.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(optionalConvention.get(), HttpStatus.OK);
+    }
+
+    // CREATE
+    @PostMapping("/convention")
+    public ResponseEntity<Convention> createConvention(@RequestBody @Valid Convention convention) {
+
+        //on force l'id à null au cas où le client en aurait fourni un
+        convention.setId(null);
+
+        conventionDao.save(convention);
+
+        return new ResponseEntity<>(convention, HttpStatus.CREATED);
+    }
+
+    // DELETE
+    @DeleteMapping("/convention/{id}")
+    public ResponseEntity<Convention> deleteConvention(@PathVariable Integer id) {
+
+        //On vérifie que l'Convention existe bien dans la base de donnée
+        Optional<Convention> optionalConvention = conventionDao.findById(id);
+
+        //si l'convention n'existe pas
+        if(optionalConvention.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        conventionDao.deleteById(id);
+
+        return new ResponseEntity<>(optionalConvention.get(), HttpStatus.OK);
+
+    }
+
+    // UPDATE
+    @PutMapping("/convention/{id}")
+    public ResponseEntity<Convention> updateConvention(@RequestBody @Valid Convention conventionEnvoye, @PathVariable Integer id) {
+
+        conventionEnvoye.setId(id);
+
+        Optional<Convention> optionalConvention = conventionDao.findById(id);
+
+        if(optionalConvention.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        conventionDao.save(conventionEnvoye);
+
+        return new ResponseEntity<>(optionalConvention.get(), HttpStatus.OK);
+
+    }
+
+}
